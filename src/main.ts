27bdb149b4b19f2fa3c77d254d08626b6aa597f4
Sahil8383 <sahilbhatt915@gmail.com>
2024-04-11 14:38:59 +0530

    Update console.log message in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('from Work-Ubantu');
+  console.log('from Work-Ubantu 1');
   await app.listen(3000);
 }
 bootstrap();
