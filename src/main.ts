e8b86f77cb3d615e247c37d80623b8d2fabe78e5
Sahil8383 <sahilbhatt915@gmail.com>
2024-04-11 21:11:28 +0530

    chore: changed message

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('from Work-Ubantu 3');
+  console.log('from Work-Ubantu 4');
   await app.listen(3000);
 }
 bootstrap();
